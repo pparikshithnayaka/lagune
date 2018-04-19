56f121437d541ac13dccd1436c12229829fda49d
Neetshin <neetshin@neetsh.in>
2018-04-19 23:04:28 +0530

    Restore login component

---
+++ src/renderer/features/login/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default class Login extends React.PureComponent {
+
+  public render () {
+    return (
+      <div className='login'>
+        Login
+      </div>
+    );
+  }
+
+}
