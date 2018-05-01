c73970e81c7310e14fa990c23cddc22ea45117a0
Neetshin <neetshin@neetsh.in>
2018-05-02 00:27:49 +0530

    Add type definition in Dexie constructor

--- src/renderer/db.ts
+++ src/renderer/db.ts
@@ -1,9 +1,40 @@
 import Dexie from 'dexie';
 
-const db = new Dexie('Lagune');
+export interface Meta {
+  /** User id of authorized user */
+  me: string;
 
-db.version(1).stores({
-  credential: '&url, token',
-});
+  /** Access token for the API */
+  access_token: string;
+
+  /** URL of host (e.g. `https://mastodon.social`) */
+  url: string;
+
+  /** Suffix of URL which respresents version of the API */
+  url_version: string;
+
+  /** URI of streaming API (e.g. `wss://mastodon.social`) */
+  streaming_url: string;
+}
+
+class LaguneDB extends Dexie {
+
+  public meta!: Dexie.Table<Meta, number>;
+
+  constructor () {
+    super('Lagune');
+    this.version(1).stores({
+      meta: [
+        '&me',
+        'access_token',
+        'url',
+        'url_version',
+        'streaming_url',
+      ].join(','),
+    });
+  }
+}
+
+const db = new LaguneDB();
 
 export default db;
