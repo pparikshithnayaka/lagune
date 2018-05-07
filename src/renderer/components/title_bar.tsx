864988d0dfc0d32b9a6195c56425712413810cc7
neet <n33t5hin@gmail.com>
2018-05-07 16:59:18 +0530

    Add back button to titleBar

--- src/renderer/components/title_bar.tsx
+++ src/renderer/components/title_bar.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
+import Button from '@/components/button';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-const TitleBar: React.SFC = ({ children }) => (
-  <div className='title-bar unselectable'>
-    <h2>{children}</h2>
-  </div>
-);
+interface Props extends RouteComponentProps<{}> {}
 
-export default TitleBar;
+class TitleBar extends React.PureComponent<Props> {
+
+  private handleGoBack = () => {
+    this.props.history.goBack();
+  }
+
+  public render () {
+    const { children } = this.props;
+
+    return (
+      <div className='title-bar unselectable'>
+        <Button
+          className='title-bar__back-button'
+          text='Back'
+          linkButton
+          onClick={this.handleGoBack}
+        >
+          <i className='fas fa-chevron-left' />
+        </Button>
+
+        <h2>{children}</h2>
+      </div>
+    );
+  }
+}
+
+export default withRouter<Props>(TitleBar);
