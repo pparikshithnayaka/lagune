201fb9ccdb52d13cd2264c7e530f0c32f3004f22
neet <n33t5hin@gmail.com>
2018-09-01 19:49:54 +0900

    Prettify status list action creator

--- src/renderer/actions/status_list.ts
+++ src/renderer/actions/status_list.ts
@@ -1,34 +1,9 @@
-import Mastodon from '@lagunehq/core';
+import { Status } from '@lagunehq/core';
 import actionCreatorFactory from 'typescript-fsa';
-
 const actionCreator = actionCreatorFactory('StatusList');
 
-export const fetchHomeTimeline = actionCreator.async<
-  {},
-  Mastodon.Status[],
-  Mastodon.Error
->('FETCH_HOME');
-
-export const fetchCommunityTimeline = actionCreator.async<
-  {},
-  Mastodon.Status[],
-  Mastodon.Error
->('FETCH_COMMUNITY');
-
-export const fetchPublicTimeline = actionCreator.async<
-  {},
-  Mastodon.Status[],
-  Mastodon.Error
->('FETCH_PUBLIC');
-
-export const fetchTagTimeline = actionCreator.async<
-  string,
-  Mastodon.Status[],
-  Mastodon.Error
->('FETCH_TAG');
-
-export const fetchListTimeline = actionCreator.async<
-  string,
-  Mastodon.Status[],
-  Mastodon.Error
->('FETCH_LIST');
+export const fetchHomeTimeline      = actionCreator.async<void, Status[]>('FETCH_HOME');
+export const fetchCommunityTimeline = actionCreator.async<void, Status[]>('FETCH_COMMUNITY');
+export const fetchPublicTimeline    = actionCreator.async<void, Status[]>('FETCH_PUBLIC');
+export const fetchTagTimeline       = actionCreator.async<string, Status[]>('FETCH_TAG');
+export const fetchListTimeline      = actionCreator.async<string, Status[]>('FETCH_LIST');
