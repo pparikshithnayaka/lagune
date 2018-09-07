f008f640eccb2ce6b0da527a3d5a6d918a901bca
neet <n33t5hin@gmail.com>
2018-09-07 12:11:43 +0900

    Improve message dialog

--- src/renderer/middlewares/error.ts
+++ src/renderer/middlewares/error.ts
@@ -2,14 +2,17 @@ import { RootAction } from '@/renderer/actions';
 import { ipcRenderer as ipc } from 'electron';
 import { Middleware } from 'redux';
 
-export default function errorsMiddleware (): Middleware {
+export default function errorsMiddleware () {
   return () => (next) => (action: RootAction) => {
-    if (/FAIL$/g.test(action.type)) {
-      if (action.payload && action.payload.error) {
-        ipc.send('show-error', action.payload.error);
-      } else {
-        ipc.send('show-error', 'An unexpected error occurred.');
-      }
+    if (/FAILED$/g.test(action.type)) {
+      const message = action.payload.toString() || 'An unexpected error occurred';
+      const link = 'https://github.com/lagunehq/lagune/issues';
+
+      ipc.send('show-message-box', {
+        type: 'error',
+        message: `Lagune: ${message}`,
+        detail: `To report bug to developer, open issue on GitHub: ${link}`,
+      });
     }
 
     return next(action);
